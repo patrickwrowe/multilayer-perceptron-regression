48c780ead8c9e3424ffd42967980052b83460610
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-03-18 21:20:19 -0230

    add dataloader functionality to return tensors

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,15 @@
 from sklearn.datasets import load_diabetes
 from torch.utils.data import Dataset
 import torch
+from torch import tensor, utils
+import torch.utils.data
 import attrs
 
 @attrs.define()
 class DiabetesDataset(Dataset):
     """PyTorch Dataset iterator for the sklearn diabetes dataset"""
 
+    batch_size: int = 64
     diabetes_dataset = load_diabetes()
     features = diabetes_dataset["data"]  # type: ignore
     labels = diabetes_dataset["target"]  # type: ignore
@@ -16,5 +19,24 @@ class DiabetesDataset(Dataset):
         return len(self.features)
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        return torch.tensor(self.features[idx], dtype=torch.float), torch.tensor(self.labels[idx], dtype=torch.float)
+        return (torch.tensor(self.features[idx], dtype=torch.float), 
+                torch.tensor(self.labels[idx], dtype=torch.float))
 
+    def get_dataloader(self, train):
+        get_tensor = lambda x: torch.tensor(x.values.astype(float),
+                                            dtype=torch.float32)
+
+        tensors = (get_tensor(self.features), get_tensor(self.labels))
+        return self.get_tensorloader(tensors, train)
+
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
+
+    def val_dataloader(self):
+        return self.get_dataloader(train=False)
+
+    def get_tensorloader(self, tensors, train, indices=slice(0, None)):
+        tensors = tuple(a[indices] for a in tensors)
+        dataset = torch.utils.data.TensorDataset(*tensors)
+        return torch.utils.data.DataLoader(dataset, self.batch_size,
+                                           shuffle=train)
